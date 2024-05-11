1bc21a7d5e7a4e79828a40a061beda81ab65a0ea
Harits-sl <harits.salsabil@gmail.com>
2024-05-11 16:11:54 +0700

    blade report page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,6 +36,13 @@
 
     <!-- Custom scripts for all pages-->
     <script src={{ asset("js/sb-admin-2.min.js") }}></script>
+
+    <!-- Page level plugins -->
+    <script src={{ asset("vendor/datatables/jquery.dataTables.min.js") }}></script>
+    <script src={{ asset("vendor/datatables/dataTables.bootstrap4.min.js") }}></script>
+
+    <!-- Page level custom scripts -->
+    <script src={{ asset("js/demo/datatables-demo.js") }}></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::Resource('/', LoginController::class);
 Route::post('/auth/login', [LoginController::class, 'login']);
+// Route::Resource('/dashboard', DashboardController::class);
+Route::Resource('/dashboard/report', ReportController::class);
