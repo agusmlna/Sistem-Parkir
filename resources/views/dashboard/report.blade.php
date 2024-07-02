2f60e861fdec5af358c0c078c09211bb59a79968
Agus Maulana <bassram71@gmail.com>
2024-07-02 23:46:03 +0700

    tambah tombol generate pdf

--- resources/views/dashboard/report.blade.php
+++ resources/views/dashboard/report.blade.php
@@ -22,8 +22,11 @@
             <div class="container-fluid">
 
                 <!-- Page Heading -->
-                <h1 class="h3 mb-2 text-gray-800">Data Laporan</h1>
-
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800 fw-bold">Data Laporan Parkiran</h1>
+                    <a href="" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>
+                        Generate Report</a>
+                </div>
                 <!-- DataTales Example -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
