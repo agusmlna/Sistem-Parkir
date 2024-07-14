9618925924389ca7bf79c813a01c4b698776e494
Harits-sl <harits.salsabil@gmail.com>
2024-07-14 13:15:24 +0700

    fix struk and pdf

--- resources/views/struk/pdfstruk.blade.php
+++ resources/views/struk/pdfstruk.blade.php
@@ -10,64 +10,56 @@
     <h1>{{ $title }}</h1>
     <p>{{ $date }}</p>
 
-    <div class="container-fluid">
-        <!-- Content Row -->
-        <div class="row">
-
-            <div class="col justify-content-center">
-                <!-- Illustrations -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3 text-center">
-                        <h2>{{ $date }}</h2>
-                    </div>
-                    <table class="table table-borderless pl-5">
-                        <thead>
-                            <tr>
-                                <th scope="col">
-                                    <h5>Nama Motor</h5>
-                                </th>
-                                <th scope="col">
-                                    <h5>Plat Nomor</h5>
-                                </th>
-                                <th scope="col">
-                                    <h5>Jam Masuk</h5>
-                                </th>
-                                <th scope="col">
-                                    <h5>Jenis Motor</h5>
-                                </th>
-                                <th scope="col">
-                                    <h5>Total Harga</h5>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <h5>{{ $data->motor }}</h5>
-                                </td>
-                                <td>
-                                    <h5>{{ $data->plat_nomor }}</h5>
-                                </td>
-                                <td>
-                                    <h5>{{ $data->jam_masuk->format('H.i') }}</h5>
-                                </td>
-                                <td>
-                                    <h5>{{ $data->jenis }}</h5>
-                                </td>
-                                <td>
-                                    <h5>{{ $data->biaya }}</h5>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div class="card-header py-3 text-center">
-                        <h3 class="m-0 font-weight-bold text-primary">
-                            Terima Kasih
-                        </h3>
-                        <h4>Penitipan motor simpati</h4>
-                    </div>
-                </div>
-            </div>
+    <!-- Illustrations -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 text-center">
+            <h2>{{ $date }}</h2>
+        </div>
+        <table class="table table-borderless">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        <h5>Nama Motor</h5>
+                    </th>
+                    <th scope="col">
+                        <h5>Plat Nomor</h5>
+                    </th>
+                    <th scope="col">
+                        <h5>Jam Masuk</h5>
+                    </th>
+                    <th scope="col">
+                        <h5>Jenis Motor</h5>
+                    </th>
+                    <th scope="col">
+                        <h5>Total Harga</h5>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <p>{{ $data->motor }}</p>
+                    </td>
+                    <td>
+                        <p>{{ $data->plat_nomor }}</p>
+                    </td>
+                    <td>
+                        <p>{{ $data->jam_masuk->format('H.i') }}</p>
+                    </td>
+                    <td>
+                        <p>{{ $data->jenis }}</p>
+                    </td>
+                    <td>
+                        <p>{{ $data->biaya }}</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="card-header py-3 text-center">
+            <h3 class="m-0 font-weight-bold text-primary">
+                Terima Kasih
+            </h3>
+            <h4>Penitipan motor simpati</h4>
         </div>
     </div>
 
--- resources/views/transaksi/pdfreport.blade.php
+++ resources/views/transaksi/pdfreport.blade.php
@@ -10,61 +10,52 @@
     <h1>{{ $title }}</h1>
     <p>{{ $date }}</p>
 
-    <div class="container-fluid">
-        <!-- Content Row -->
-        <div class="row">
 
-            <div class="col-12">
-                <!-- Illustrations -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3 text-center">
-                        <h4>{{ $date }}</h4>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Motor</th>
-                                    <th>Plat Nomor</th>
-                                    <th>Jam Masuk</th>
-                                    <th>Jenis Motor</th>
-                                    <th>Biaya</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php
-                                    $i = 1;
-                                    $total = 0;
-                                @endphp
-                                @foreach ($data as $d)
-                                    @php
-                                        $total += $d->biaya;
-                                    @endphp
-                                    <tr>
-                                        <td>{{ $i++ }}</td>
-                                        <td>{{ $d->motor }}</td>
-                                        <td>{{ $d->plat_nomor }}</td>
-                                        <td>{{ $d->jam_masuk->format('d, M:H.i') }}</td>
-                                        <td>{{ $d->jenis }}</td>
-                                        <td>Rp. {{ $d->biaya }}</td>
-                                        <td>{{ $d->tipe_pembayaran }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th scope="row">Total</th>
-                                    <td colspan="4">Rp.{{ $total }}</td>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-            </div>
+    <!-- Illustrations -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 text-center">
+            <h4>{{ $date }}</h4>
         </div>
+        <table class="table table-bordered" id="dataTable" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Motor</th>
+                    <th>Plat Nomor</th>
+                    <th>Jam Masuk</th>
+                    <th>Jenis Motor</th>
+                    <th>Biaya</th>
+                    <th>Tipe Pembayaran</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $i = 1;
+                    $total = 0;
+                @endphp
+                @foreach ($data as $d)
+                    @php
+                        $total += $d->biaya;
+                    @endphp
+                    <tr>
+                        <td>{{ $i++ }}</td>
+                        <td>{{ $d->motor }}</td>
+                        <td>{{ $d->plat_nomor }}</td>
+                        <td>{{ $d->jam_masuk->format('d, M:H.i') }}</td>
+                        <td>{{ $d->jenis }}</td>
+                        <td>Rp. {{ $d->biaya }}</td>
+                        <td>{{ $d->tipe_pembayaran }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th scope="row">Total</th>
+                    <td colspan="6">Rp.{{ $total }}</td>
+                </tr>
+            </tfoot>
+        </table>
     </div>
-
 </body>
 
 </html>
